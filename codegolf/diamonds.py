5f3384b06eaa1a861accbd619633f54896242e48
jbar-ry <james.barry26@mail.dcu.ie>
2022-07-07 14:43:59 +0100

    add diamonds solution

---
+++ codegolf/diamonds.py
@@ -0,0 +1,23 @@
+M=9
+for i in range(1,10):
+    for c in range(1,i+1):
+        if c>1:
+            n=c-1
+            v=[]
+            while n>=1:v.append(n);n-=1
+            l="".join(str(x) for x in v[::-1])
+            r="".join(str(x) for x in v)
+            p=l+str(c)+r
+            print(" "*(M-c),p)
+        else:
+            print(" "*(M-c),c)
+    for k in range(1,i):
+        c=i-k
+        n=c-1
+        v=[]
+        while n>=1:v.append(n);n-=1
+        l="".join(str(x) for x in v[::-1])
+        r="".join(str(x) for x in v)
+        p=l+str(c)+r
+        print(" "*(M-c),p) 
+    print()
